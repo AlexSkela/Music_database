203e705bcfa3dbb73e2f369ba64dd81f755dabde
Alexandra Kustova <avkustova@inbox.ru>
2021-08-03 00:02:45 +0300

    Done 2.0

--- main.py
+++ main.py
@@ -33,39 +33,49 @@ GROUP BY id_genre
 ORDER BY COUNT(id_singer)""").fetchmany(10))
 
 pprint(connection.execute("""SELECT namealbum, COUNT(t.nameoftrack) FROM album a
-INNER JOIN track t ON a.id = t.id_album
+JOIN track t ON a.id = t.id_album
 WHERE yearofrelease BETWEEN 2019 AND 2020
 GROUP BY namealbum""").fetchmany(10))
 
 pprint(connection.execute("""SELECT namealbum, AVG(t.duration) FROM album a
-INNER JOIN track t ON a.id = t.id_album
+JOIN track t ON a.id = t.id_album
 GROUP BY namealbum""").fetchmany(10))
 
 pprint(connection.execute("""SELECT s.name FROM singer s
-INNER JOIN albumsinger asi ON s.id = asi.id_singer
-INNER JOIN album al ON asi.id_album = al.id
+JOIN albumsinger asi ON s.id = asi.id_singer
+JOIN album al ON asi.id_album = al.id
 WHERE yearofrelease != 2020""").fetchmany(10))
 
 pprint(connection.execute("""SELECT c.name_collection FROM collection c
-INNER JOIN collectiontrack ct ON c.id = ct.id_collection
-INNER JOIN track tr ON ct.id_track = tr.id
-INNER JOIN album ON tr.id_album = album.id
-INNER JOIN albumsinger als ON album.id = als.id_album
-INNER JOIN singer si ON als.id_singer = si.id
+JOIN collectiontrack ct ON c.id = ct.id_collection
+JOIN track tr ON ct.id_track = tr.id
+JOIN album ON tr.id_album = album.id
+JOIN albumsinger als ON album.id = als.id_album
+JOIN singer si ON als.id_singer = si.id
 WHERE name = 'Баста'""").fetchmany(10))
+
+pprint(connection.execute("""SELECT namealbum FROM album al
+JOIN albumsinger alsi ON al.id = alsi.id_album
+JOIN singer sin ON alsi.id_singer = sin.id
+JOIN gendersinger gensi ON sin.id = gensi.id_singer
+GROUP BY namealbum
+HAVING COUNT(gensi.id_genre) > 1""").fetchmany(10))
 
 pprint(connection.execute("""SELECT nameoftrack FROM track
 LEFT JOIN collectiontrack ct ON track.id = ct.id_track
 WHERE id_collection IS NULL""").fetchmany(10))
 
 pprint(connection.execute("""SELECT name, tr.duration FROM singer
-INNER JOIN albumsinger albsin ON singer.id = albsin.id_singer
-INNER JOIN album alb ON albsin.id_album = alb.id
-INNER JOIN track tr ON alb.id = tr.id
+JOIN albumsinger albsin ON singer.id = albsin.id_singer
+JOIN album alb ON albsin.id_album = alb.id
+JOIN track tr ON alb.id = tr.id_album
 GROUP BY singer.name, tr.duration
-ORDER BY tr.duration ASC""").fetchmany(1))
+HAVING tr.duration = (
+SELECT MIN(duration) FROM track)""").fetchmany(10))
 
 pprint(connection.execute("""SELECT namealbum, COUNT(tra.id_album) FROM album
-INNER JOIN track tra ON album.id = tra.id_album
+JOIN track tra ON album.id = tra.id_album
 GROUP BY namealbum, tra.id_album
-HAVING COUNT(tra.id_album) = 1""").fetchmany(3))
+HAVING COUNT(tra.id_album) = (
+SELECT MIN(id_album) FROM track)""").fetchmany(10))
+
